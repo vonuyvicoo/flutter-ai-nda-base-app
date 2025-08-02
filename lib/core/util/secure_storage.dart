bf6aea72ad49299d2475d9e1555cc358ed7382eb
vonuyvicoo <vonuyvico@gmail.com>
2025-08-02 16:41:43 +0800

    API

--- lib/core/util/secure_storage.dart
+++ lib/core/util/secure_storage.dart
@@ -3,7 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class SecureStorage {
   static const _storage = FlutterSecureStorage();
 
-  static const _apiKey = 'AIzaSyCGc070iYIVDIxopXAsosIjXziCKKLnHjs';
+  static const _apiKey = 'xx';
 
   Future<void> storeApiKey(String apiKey) async {
     await _storage.write(key: _apiKey, value: apiKey);
